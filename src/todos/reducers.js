604ba74d7f29381b87b8861b5930683a4e7692d6
Pablo Velez <pvelez@bp-3.com>
2020-08-11 23:30:31 +0200

    add the selectors file to abstract how we get the state values

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -7,47 +7,46 @@ import {
   LOAD_TODOS_FAILURE,
 } from "./actions";
 
-export const isLoading = (state = false, action) => {
-  const { type } = action;
-
-  switch (type) {
-    case LOAD_TODOS_IN_PROGRESS:
-      return true;
-    case LOAD_TODOS_SUCCESS:
-    case LOAD_TODOS_FAILURE:
-      return false;
-    default:
-      return state;
-  }
+const initialState = {
+  isLoading: false,
+  data: [],
 };
 
-export const todos = (state = [], action) => {
+export const todos = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case CREATE_TODO: {
       const { todo } = payload;
-      return state.concat(todo);
+      return { ...state, data: state.data.concat(todo) };
     }
     case REMOVE_TODO: {
       const { todo: todoToRemoved } = payload;
-      return state.filter((todo) => todo.id !== todoToRemoved.id);
+      return {
+        ...state,
+        data: state.data.filter((todo) => todo.id !== todoToRemoved.id),
+      };
     }
     case MARK_TODO_AS_COMPLETED: {
       const { todo: completedTodo } = payload;
-      return state.map((todo) => {
-        if (todo.id === completedTodo.id) {
-          return completedTodo;
-        }
-        return todo;
-      });
+      return {
+        ...state,
+        data: state.data.map((todo) => {
+          if (todo.id === completedTodo.id) {
+            return completedTodo;
+          }
+          return todo;
+        }),
+      };
     }
     case LOAD_TODOS_SUCCESS: {
       const { todos } = payload;
-      return todos;
+      return { ...state, isLoading: false, data: todos };
     }
     case LOAD_TODOS_IN_PROGRESS:
+      return { ...state, isLoading: true };
     case LOAD_TODOS_FAILURE:
+      return { ...state, isLoading: false };
     default:
       return state;
   }
